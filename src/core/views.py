abd4b1402eeff88b46388454a966f9bb3c84d405
leandrito <moonkyhead@hotmail.com>
2024-11-28 11:02:41 -0300

    Dentro de template, se crea core y ahi el archivo html

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import Context, Template
+
+def saludar(request):
+	return HttpResponse("Rompan todooooooooooo!")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1> Esto tiene todo el sentido del mundo? <h1/>")
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.capitalize()
+    return HttpResponse(f"{apellido}, {nombre}")
